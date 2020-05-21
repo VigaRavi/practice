5afdf5ce471cd80ebb44ab3bf55f42f4b6164ae2
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2020-05-21 09:01:20 +0530

    Add files via upload

---
+++ bool data type.c
@@ -0,0 +1,21 @@
+/*#include<stdio.h>
+#include<stdbool.h>
+  int main()
+  {
+  	bool a= true;
+  	if(4>3)
+  	printf("")
+  	return 0;
+  }*/
+  
+ 
+  
+  #include<stdio.h>
+  #include<complex.h>
+  
+  int main()
+  {
+  	int complex z= 1+5*2;
+  	printf("%d",3*z);
+  	return 0;
+  }
